0c15f5c68374ef6f4d1189a345a139cb82d0e540
SamriddhiB <110332382+SamriddhiB@users.noreply.github.com>
2022-07-31 17:49:27 +0530

    Add files via upload

---
+++ prac6.java
@@ -0,0 +1,48 @@
+/*Given an array of strings, return a new array without the strings that are equal to the target string. One approach is to count the occurrences of the target string, make a new array of the correct length, and then copy over the correct strings.
+wordsWithout(["a", "b", "c", "a"], "a") → ["b", "c"]
+wordsWithout(["a", "b", "c", "a"], "b") → ["a", "c", "a"]
+wordsWithout(["a", "b", "c", "a"], "c") → ["a", "b", "a"] */
+
+
+import java.util.*;
+public class prac6 {
+   
+    public static void  wordsWithout(String[] a,String b)
+    {
+        int count=0;
+        for (int i = 0; i < a.length; i++) {
+            if(a[i].equals(b))
+            {
+                count++;
+            }
+        }
+        int l=a.length-count;
+        System.out.println("NEW ARRAY:");
+        for(int i=0;i<a.length;i++)
+        {
+                if(a[i].compareTo(b)!=0)
+                {
+                   System.out.println(a[i]);
+                
+                }
+        }
+       
+        
+    }
+    public static void main(String args[])
+    {
+    Scanner s= new Scanner(System.in);
+    System.out.println("Enter length:");
+    int n=s.nextInt();
+    System.out.println("Enter elements:");
+    String s1[]=new String[n];
+    for(int i=0;i<(n);i++)
+    {
+        s1[i]=s.next();
+    }
+    System.out.println("Enter string :");
+    String s2=s.next();
+    wordsWithout(s1,s2);
+    s.close();
+    }
+}
